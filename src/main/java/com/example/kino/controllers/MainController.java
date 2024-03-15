5c865e0ce6211c7754420799246f78bcbaa6684e
andreiazarimelnitsuk <162023766+andreiazarimelnitsuk@users.noreply.github.com>
2024-03-15 15:02:23 +0200

    translate

--- src/main/java/com/example/kino/controllers/MainController.java
+++ src/main/java/com/example/kino/controllers/MainController.java
@@ -10,12 +10,12 @@ public class MainController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("title", "Главная страница");
+        model.addAttribute("title", "Kino koduleht");
         return "home";
     }
     @GetMapping("/about")
     public String about(Model model) {
-        model.addAttribute("title", "Страница про нас");
+        model.addAttribute("title", "Meie leht");
         return "about";
     }
 }
